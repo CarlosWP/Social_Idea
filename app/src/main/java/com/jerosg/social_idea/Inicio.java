1d4ad5b6ec4f7be38321face59aad18b37fb385e
Upsetting Boy <JeronimoSG@hotmail.es>
2016-08-05 20:48:19 +0200

    Initial commit

---
+++ app/src/main/java/com/jerosg/social_idea/Inicio.java
@@ -0,0 +1,13 @@
+package com.jerosg.social_idea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
